6ed56004dbf2fc1705956bf8ebc5cf95ca2629e7
0x64656164 <124457482+0x64656164@users.noreply.github.com>
2026-03-07 04:37:24 +0300

    Update REGEXP_FILTER to include YA and VK

--- updater.py
+++ updater.py
@@ -9,7 +9,7 @@ from base import SingBoxProxy
 
 # --- НАСТРОЙКИ ---
 SUB_LINK = 'https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt'
-REGEXP_FILTER = r'^(?!.*Russia).*$'
+REGEXP_FILTER = r'^(?=.*(?:YA|VK))(?!.*Russia).*$'
 GITHUB_RAW_BASE = 'https://raw.githubusercontent.com/0x64656164/cancer-treatment/refs/heads/main/ruleset/srs/'
 
 TOP_COUNT = 100
